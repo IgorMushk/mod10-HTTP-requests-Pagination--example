b22536dca9cb928023c86ebb16c2e0c3d6dfd9d1
IgorMushk <igor.mushketik@gmail.com>
2023-08-09 20:05:04 +0300

    error на Git
    Mixed Content: The page at '<URL>' was loaded over HTTPS, but requested an insecure resource '<URL>'. This request has been blocked; the content must be served over HTTPS.

--- src/js/weather-ar.js
+++ src/js/weather-ar.js
@@ -3,32 +3,30 @@
 // https://swapi.dev/
 
 // Перерва до  21.00
-const search = document.querySelector(".js-search");
-const list = document.querySelector(".js-list");
-search.addEventListener("submit", onSearch);
+const search = document.querySelector('.js-search');
+const list = document.querySelector('.js-list');
+search.addEventListener('submit', onSearch);
 
 function onSearch(evt) {
   evt.preventDefault();
 
   const { query, days } = evt.currentTarget.elements;
   getWeather(query.value, days.value)
-    .then((data) => (list.innerHTML = createMarkup(data.forecast.forecastday)))
-    .catch((err) => console.log(err));
+    .then(data => (list.innerHTML = createMarkup(data.forecast.forecastday)))
+    .catch(err => console.log(err));
 }
 
 function getWeather(city, days) {
-  const BASE_URL = "http://api.weatherapi.com/v1";
-  const API_KEY = "ce2cb9b2a3da414bb5b172546231704";
+  const BASE_URL = 'https://api.weatherapi.com/v1';
+  const API_KEY = 'ce2cb9b2a3da414bb5b172546231704';
   const params = new URLSearchParams({
-    key : API_KEY,
+    key: API_KEY,
     q: city,
     days: days,
-    lang: 'uk'
+    lang: 'uk',
   });
 
-  return fetch(
-    `${BASE_URL}/forecast.json?${params}`
-  ).then((resp) => {
+  return fetch(`${BASE_URL}/forecast.json?${params}`).then(resp => {
     if (!resp.ok) {
       throw new Error(resp.statusText);
     }
@@ -53,5 +51,5 @@ function createMarkup(arr) {
     <h3>${avgtemp_c}</h3>
 </li>`
     )
-    .join("");
+    .join('');
 }
